d2c46bea84a21a18aa8a7420f543211cf1f88218
Arturo Bassoli <bassoliarturo@gmail.com>
2022-05-12 17:03:29 +0200

    reverseString consegna

--- reverse-string-start.js
+++ reverse-string-start.js
@@ -1,5 +1,11 @@
 /**
-	* Data una stringa, restituire una stringa con i caratteri in ordine inverso.
-	* Esempio:
-	* reverse("ciao") => "oaic"
-	*/
+ * Data una stringa, restituire una stringa con i caratteri in ordine inverso.
+ * Esempio:
+ * reverse("ciao") => "oaic"
+ */
+
+function reverseString(parola) {
+  return parola.split('').reverse().join('')
+}
+
+reverseString('ciao')
