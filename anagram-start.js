468238043554431891617bc416e6ab578987692f
Arturo Bassoli <bassoliarturo@gmail.com>
2022-05-12 17:29:37 +0200

    anagramStart consegna

--- anagram-start.js
+++ anagram-start.js
@@ -1,7 +1,30 @@
 /**
-	* Scrivere una funzione che controlli se due parole sono anagrammi e mostrare
-	* il risultato in console.
-	* Esempio
-	* isAnagram("cinema", "iceman") => true
-	* isAnagram("manuel", "giraffa") => false
-	*/
+ * Scrivere una funzione che controlli se due parole sono anagrammi e mostrare
+ * il risultato in console.
+ * Esempio
+ * isAnagram("cinema", "iceman") => true
+ * isAnagram("manuel", "giraffa") => false
+ */
+
+/**
+ * Controlla se due stringhe sono anagrammi
+ * @param {String} firstWord prima parola da confrontare
+ * @param {String} secondWord seconda parola da confrontare
+ * @returns true or false se le parole sono anagrammi o meno
+ */
+function isAnagram (firstWord, secondWord) {
+  let firstWordSorted = sortWord(firstWord);
+  let secondWordSorted = sortWord(secondWord);
+  return firstWordSorted === secondWordSorted
+}
+
+/**
+ * Data una parola ritorna una stringa con le lettere in ordine alfabetico
+ * @param {String} word parola di cui bisogna ordinare le lettere
+ * @returns parola con le lettere ordinate
+ */
+function sortWord (word) {
+  return word.split('').sort().join('');
+}
+
+isAnagram("cinema", "iceman")
