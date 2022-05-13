cbd0a2d51e9a7e7f0e4ea9a041ba5ea818b8a270
Arturo Bassoli <bassoliarturo@gmail.com>
2022-05-13 23:46:53 +0200

    maxcharStart consegna

--- maxchar-start.js
+++ maxchar-start.js
@@ -1,6 +1,35 @@
 /**
-	* Data una stringa, restituisci il carattere che viene ripetuto più volte.
-	* Esempio
-	* maxChar("abccccccd") => "c"
-	* maxChar("apple 12311111") => "1"
-	*/
+ * Data una stringa, restituisci il carattere che viene ripetuto più volte.
+ * Esempio
+ * maxChar("abccccccd") => "c"
+ * maxChar("apple 12311111") => "1"
+ */
+
+/**
+ *Ritorna l'occorrenza maggiore della stringa
+ * @param {String} word stringa di cui ritornare l'occorrenza maggiore
+ */
+function countChar(word) {
+  const wordToArray = word.split("");
+  const letterCount = {};
+
+  wordToArray.forEach(function (element) {
+    if (letterCount[element]) {
+      ++letterCount[element];
+    } else {
+      letterCount[element] = 1;
+    }
+  });
+
+  Object.keys(letterCount).reduce((pv, cv) => {
+    if (!pv) {
+      return cv;
+    } else if (letterCount[pv] < letterCount[cv]) {
+      return cv;
+    }
+
+    return pv;
+  });
+}
+
+countChar("abccccccd");
