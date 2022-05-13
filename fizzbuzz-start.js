41253143ab5f21395a9272d9a5310768ac883ed7
Arturo Bassoli <bassoliarturo@gmail.com>
2022-05-13 13:19:05 +0200

    fizzBuzzStart consegna

--- fizzbuzz-start.js
+++ fizzbuzz-start.js
@@ -1,6 +1,17 @@
 /**
-	* Scrivere una funzione che mostri in console una serie di numeri da 1 a 100.
-	* Per ogni multiplo di 3 dovrà mostrare "Fizz", mentre per ogni multiplo di 5
-	* dovrà mostrare "Buzz". Nel caso il numero sia multiplo di entrambi dovrà
-	* mostrare "FizzBuzz".
-	*/
+ * Scrivere una funzione che mostri in console una serie di numeri da 1 a 100.
+ * Per ogni multiplo di 3 dovrà mostrare "Fizz", mentre per ogni multiplo di 5
+ * dovrà mostrare "Buzz". Nel caso il numero sia multiplo di entrambi dovrà
+ * mostrare "FizzBuzz".
+ */
+
+function fizzBuzz(firstNum, secondNum) {
+  for (let i = 1; i < 101; i++) {
+    if (i % firstNum == 0 && i % secondNum == 0) console.log("FizzBuzz");
+    else if (i % firstNum == 0) console.log("Fizz");
+    else if (i % secondNum == 0) console.log("Buzz");
+    else console.log(i);
+  }
+}
+
+fizzBuzz(3, 5)
