661a96ce43e148995f47a7da4dbd4968f1c29b73
Arturo Bassoli <bassoliarturo@gmail.com>
2022-05-13 02:53:15 +0200

    reverseIntergetStart consegna

--- reverse-interget-start.js
+++ reverse-interget-start.js
@@ -1,8 +1,26 @@
 /**
-	* Dato un numero intero restituire il numero inverso
-	* Esempio
-	* reverseInt(15) => 51
-	* reverseInt(981) => 189
-	* reverseInt(-90) => -9
-	* reverseInt(-15) => -51
-	*/
+ * Dato un numero intero restituire il numero inverso
+ * Esempio
+ * reverseInt(15) => 51
+ * reverseInt(981) => 189
+ * reverseInt(-90) => -9
+ * reverseInt(-15) => -51
+ */
+
+/**
+ * Dato un numero ritorna il numero invertito mantenendo il segno
+ * @param {Integer} num numero da invertire
+ * @returns numero invertito
+ */
+function reverseInt(num) {
+  let numToArray = num.toString().split("").reverse();
+
+  if (num < 0) {
+    numToArray.pop();
+    return Number(-numToArray.join(""));
+  }
+
+  return Number(numToArray.join(""));
+}
+
+reverseInt(-90);
